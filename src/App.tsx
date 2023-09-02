65eb4cf26f1fddaa1260616448b21d01d1a90861
Yuhanna Kapali <yuhannakapali@gmail.com>
2023-09-02 11:23:07 +0545

    added new design

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,53 @@
-
+import Table from "./components/Table"
+import Modal from "./components/Modal"
+import { useState } from "react"
 
 function App() {
 
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+
+    setModalOpen(true);
+  }
+
+  const toggleModal = () => {
+    setModalOpen(!modalOpen)
+  }
+
   return (
     <>
-      <div className="flex flex-col">
-        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-            <div className="overflow-hidden">
-              <table className="min-w-full text-left text-sm font-light">
-                <thead className="border-b font-medium dark:border-neutral-500">
-                  <tr>
-                    <th scope="col" className="px-6 py-4">#</th>
-                    <th scope="col" className="px-6 py-4">First</th>
-                    <th scope="col" className="px-6 py-4">Last</th>
-                    <th scope="col" className="px-6 py-4">Handle</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr className="border-b dark:border-neutral-500">
-                    <td className="whitespace-nowrap px-6 py-4 font-medium">1</td>
-                    <td className="whitespace-nowrap px-6 py-4">Mark</td>
-                    <td className="whitespace-nowrap px-6 py-4">Otto</td>
-                    <td className="whitespace-nowrap px-6 py-4">@mdo</td>
-                  </tr>
-                  <tr className="border-b dark:border-neutral-500">
-                    <td className="whitespace-nowrap px-6 py-4 font-medium">2</td>
-                    <td className="whitespace-nowrap px-6 py-4">Jacob</td>
-                    <td className="whitespace-nowrap px-6 py-4">Thornton</td>
-                    <td className="whitespace-nowrap px-6 py-4">@fat</td>
-                  </tr>
-                  <tr className="border-b dark:border-neutral-500">
-                    <td className="whitespace-nowrap px-6 py-4 font-medium">3</td>
-                    <td className="whitespace-nowrap px-6 py-4">Larry</td>
-                    <td className="whitespace-nowrap px-6 py-4">Wild</td>
-                    <td className="whitespace-nowrap px-6 py-4">@twitter</td>
-                  </tr>
-                </tbody>
-              </table>
+      <div className="w-full h-screen bg-gray-100">
+        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div className="flex flex-col">
+            <div className="mb-4 mt-4">
+              <h1 className="text-3xl font-bolder leading-tight text-gray-900">Products</h1>
+            </div>
+            <div className="-mb-2 py-4 flex flex-wrap flex-grow justify-between">
+              <div className="flex items-center py-2">
+                <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-searcg" type="text" placeholder="Search" />
+              </div>
+              <div className="flex items-center py-2">
+                <button
+                  onClick={openModal}
+                  className="inline-block px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline">
+                  New Product
+                </button>
+              </div>
+            </div>
+            <div className="-my-2 py-2 sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+              <div className="align-middle inline-block w-full shadow overflow-x-auto sm:rounded-lg border-b border-gray-200">
+                <Table />
+              </div>
             </div>
           </div>
         </div>
       </div>
+      <Modal isOpen={modalOpen} toggle={toggleModal} />
     </>
+
+
+
   )
 }
 
---
+++ src/components/Table.tsx
@@ -0,0 +1,172 @@
+
+
+function Table() {
+
+  return (
+    <>
+      <table className="min-w-full">
+        <thead>
+          <tr className="bg-gray-50 border-b border-gray-200 text-xs leading-4 text-gray-500 uppercase tracking-wider">
+            <th className="px-6 py-3 text-left font-medium">
+              <input className="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out" type="checkbox" />
+            </th>
+            <th className="px-6 py-3 text-left font-medium">
+              Name
+            </th>
+            <th className="px-6 py-3 text-left font-medium">
+              Image
+            </th>
+            <th className="px-6 py-3 text-left font-medium">
+              Price
+            </th>
+            <th className="px-6 py-3 text-left font-medium">
+              Description
+            </th>
+            <th className="px-6 py-3 text-left font-medium">
+              Actions
+            </th>
+            <th className="px-6 py-3 text-left font-medium">
+            </th>
+          </tr>
+        </thead>
+        <tbody className="bg-white">
+          <tr>
+            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+              <input className="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out" type="checkbox" />
+            </td>
+            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+              <div className="text-sm leading-5 text-gray-900">
+                page.name
+              </div>
+            </td>
+            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+
+              <div className="flex items-center">
+                <div className="flex-shrink-0 h-10 w-10">
+                  <img className="h-10 w-10 rounded-full"
+                    src="https://via.placeholder.com/400x400"
+                    alt="" />
+                </div>
+                <div className="ml-4">
+                  <div className="text-sm leading-5 font-medium text-gray-900">
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+              <div className="text-sm leading-5 text-gray-900">
+                page.slug
+              </div>
+            </td>
+            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+              <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                published
+              </span>
+            </td>
+            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+              page.created_at
+            </td>
+            <td className="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
+              <a href="#"
+                className="text-indigo-600 hover:text-indigo-900 focus:outline-none focus:underline" >
+                Delete
+              </a>
+            </td>
+          </tr>
+
+          <tr className="bg-gray-100">
+            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+              <input className="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out" type="checkbox" />
+            </td>
+            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+              <div className="text-sm leading-5 text-gray-900">
+                page.name
+              </div>
+            </td>
+            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+
+              <div className="flex items-center">
+                <div className="flex-shrink-0 h-10 w-10">
+                  <img className="h-10 w-10 rounded-full"
+                    src="https://via.placeholder.com/400x400"
+                    alt="" />
+                </div>
+                <div className="ml-4">
+                  <div className="text-sm leading-5 font-medium text-gray-900">
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+              <div className="text-sm leading-5 text-gray-900">
+                page.slug
+              </div>
+            </td>
+            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+              <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
+                Inactive
+              </span>
+            </td>
+            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+              page.created_at
+            </td>
+            <td className="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
+              <a href="#"
+                className="text-indigo-600 hover:text-indigo-900 focus:outline-none focus:underline" >
+                Delete
+              </a>
+            </td>
+          </tr>
+
+          <tr>
+            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+              <input className="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out" type="checkbox" />
+            </td>
+            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+              <div className="text-sm leading-5 text-gray-900">
+                page.name
+              </div>
+            </td>
+            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+
+              <div className="flex items-center">
+                <div className="flex-shrink-0 h-10 w-10">
+                  <img className="h-10 w-10 rounded-full"
+                    src="https://via.placeholder.com/400x400"
+                    alt="" />
+                </div>
+                <div className="ml-4">
+                  <div className="text-sm leading-5 font-medium text-gray-900">
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+              <div className="text-sm leading-5 text-gray-900">
+                page.slug
+              </div>
+            </td>
+            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+              <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">
+                draft
+              </span>
+            </td>
+            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+              page.created_at
+            </td>
+            <td className="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
+              <a href="#"
+                className="text-indigo-600 hover:text-indigo-900 focus:outline-none focus:underline" >
+                delete
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+
+
+  )
+}
+
+export default Table
